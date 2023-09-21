6ee9eb35d91c751806faaa845da227ac4c403276
OKprincess <144990952+OKprincess@users.noreply.github.com>
2023-09-21 16:22:38 +0900

    P1

---
+++ main.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  03.1
+//
+//  Created by ok sojoung on 2023/09/21.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int   input_int;
+    float input_float;
+    
+    printf("enter an integer : ");
+    scanf("%d", &input_int);
+    
+    printf("enter a float : ");
+    scanf("%f", &input_float);
+    
+    printf("integer : %d, float : %f\n", input_int, input_float);
+    
+    return 0;
+}
